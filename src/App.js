680094df4f8184699d86e7d1e059ad5f8f073381
Pushpendra Chaudhary <pushpendrac559@gmail.com>
2023-08-26 13:25:51 +0530

    Added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LiveSearch from "./Component/LiveSearch";
+
+
+const App = () => {
+
+    return(
+        <div>
+            <LiveSearch />
+        </div>
+    )
+}
+
+export default App;
